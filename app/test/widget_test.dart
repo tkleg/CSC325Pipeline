d06e443be83c02bb6d722a20f48cfd5ab9973f85
tkleg <thkleger@gmail.com>
2024-03-21 21:48:33 +0000

    put characters to strings

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -11,20 +11,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets("Counter increments smoke test", (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that the turn starts at X.
-    expect(find.text('Current Turn: X'), findsOneWidget);
-    expect(find.text('Current Turn: O'), findsNothing);
+    expect(find.text("Current Turn: X"), findsOneWidget);
+    expect(find.text("Current Turn: O"), findsNothing);
 
     // Tap a square.
     await tester.tap(find.byIcon(Icons.square));
     await tester.pump();
 
     // Verify that the turn has changed to O.
-    expect(find.text('Current Turn: O'), findsNothing);
-    expect(find.text('Current Turn: X'), findsOneWidget);
+    expect(find.text("Current Turn: O"), findsNothing);
+    expect(find.text("Current Turn: X"), findsOneWidget);
   });
 }
