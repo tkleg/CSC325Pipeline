80e92711b5da1dc8c92aa4ac1a5976d85c314c45
tkleg <thkleger@gmail.com>
2024-03-21 21:41:51 +0000

    changed widget_test.dart to test tictactoe

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -16,15 +16,15 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Current Turn: X'), findsOneWidget);
+    expect(find.text('Current Turn: O'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.square));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Current Turn: O'), findsNothing);
+    expect(find.text('Current Turn: X'), findsOneWidget);
   });
 }
